2f889ebddcaae5242997b96dbacb290a06cdc962
Gorjan <gorjanivanovskii@gmail.com>
2023-02-05 00:52:55 +0100

    Added content to Content View

--- EdenMoodTracker/Views/ContentView.swift
+++ EdenMoodTracker/Views/ContentView.swift
@@ -11,20 +11,47 @@ import CoreData
 struct ContentView: View {
     
     @State private var showingAddView = false
-
     
     var body: some View {
-        Button("Log Your Mood for Today"){
-            showingAddView.toggle()
+        
+        ZStack {
+            ZStack{
+                Circle()
+                    .foregroundColor(Color.clear)
+                    .frame(width: 350)
+                    .background(
+                            .ultraThinMaterial,
+                            in: Circle())
+                VStack(){
+                    Text("Today")
+                        .fontWeight(.light)
+                        .font(.system(size: 24))
+                        .foregroundColor(Color("edenIndigo"))
+                    
+                    Text(Date.now.formatted(.dateTime.weekday(.wide)))
+                        .foregroundColor(Color("edenIndigo"))
+                        .fontWeight(.bold)
+                        .font(.system(size: 36))
+                    
+                    Text(Date.now.formatted(.dateTime.day().month().year()))
+                        .foregroundColor(Color("edenIndigo"))
+                        .fontWeight(.black)
+                        .font(.system(size: 40))
+                    
+                    Button("Log Your Mood for Today"){
+                        showingAddView.toggle()
+                    }
+                    .frame(maxWidth: 200)
+                    .foregroundColor(Color(.white))
+                    .padding()
+                    .background(Color("edenIndigo"))
+                    .clipShape(Capsule())
+                    .sheet(isPresented: $showingAddView){
+                        AddMoodVIew()
+                    }
+                }
+            }
         }
-        .sheet(isPresented: $showingAddView){
-            AddMoodVIew()
-        }
-        .frame(maxWidth: .infinity)
-        .foregroundColor(Color(.white))
-        .padding()
-        .background(Color("edenIndigo"))
-        .clipShape(Capsule())
     }
     
     struct ContentView_Previews: PreviewProvider{
